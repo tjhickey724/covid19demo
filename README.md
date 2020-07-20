ea64af7f018d0dd9aea271fa8b55713fb67ece0a
tim <tjhickey@brandeis.edu>
2020-07-20 14:53:37 -0400

    started refactoring code

--- README.md
+++ README.md
@@ -1,27 +1,73 @@
 # covid19demo
-This is a Flask app which uses chart.js to visualize the data from http://covidtracking.com
+This is a Jupyter lab demo to visualize the data from http://covidtracking.com
 
-## Running the webserver code
-You can run the code by downloading it, unzipping it, and giving the command
-``` linux
-% python webserver_covid19.py
-```
-which will start the webserver on your computer.
-You can then view the data by going to the URL http://127.0.0.1:4000
-You will need to install the flask module to get this to work.
-Use either
-``` linux
-% conda install flask
-or
-% pip install flask
-```
-and if neither of these look familiar, then google "installing python packages" to learn how to do this.
 
 
 ## Running the Jupyter notebook code
-You can also run the Jupyter notebook code by downloading this repository, cd'ing to the jupyterdemo folder
+You can  run the Jupyter notebook code by downloading this repository, cd'ing to the jupyterdemo folder
 and giving the command
 ``` linux
-% jupyter notebook
+% jupyter lab
 ```
 You will have to have installed the Jupyter lab modules... Google is your friend, ask her how to do it!
+
+## covid19.py package
+The covid19.py module defines several functions to help you create visualizations using matplotlib
+We list them below:
+
+''' python
+getCovidData()
+'''
+This visits the two URLs for the daily historical data for the individual states and the US as a whole
+https://covidtracking.com/api/states/daily
+https://covidtracking.com/api/us/daily
+and converts them from JSON to python. It returns a list of two elements [stateData,usData]
+each of these is a list of Python objects and you can get the keys using
+''' python
+data = get_covid_data()
+statedatum = data[0][0]
+usdataum = data[1][0]
+print("state keys")
+print(list(statedatum))
+print("us keys")
+print(list(data[1][0]))
+'''
+As of 7/20/2020 the state keys are
+
+['date', 'state', 'positive', 'negative', 'pending', 'hospitalizedCurrently', 'hospitalizedCumulative', 'inIcuCurrently', 'inIcuCumulative', 'onVentilatorCurrently', 'onVentilatorCumulative', 'recovered', 'dataQualityGrade', 'lastUpdateEt', 'dateModified', 'checkTimeEt', 'death', 'hospitalized', 'dateChecked', 'totalTestsViral', 'positiveTestsViral', 'negativeTestsViral', 'positiveCasesViral', 'deathConfirmed', 'deathProbable', 'fips', 'positiveIncrease', 'negativeIncrease', 'total', 'totalTestResults', 'totalTestResultsIncrease', 'posNeg', 'deathIncrease', 'hospitalizedIncrease', 'hash', 'commercialScore', 'negativeRegularScore', 'negativeScore', 'positiveScore', 'score', 'grade']
+
+and the US keys are
+
+['date', 'states', 'positive', 'negative', 'pending', 'hospitalizedCurrently', 'hospitalizedCumulative', 'inIcuCurrently', 'inIcuCumulative', 'onVentilatorCurrently', 'onVentilatorCumulative', 'recovered', 'dateChecked', 'death', 'hospitalized', 'lastModified', 'total', 'totalTestResults', 'posNeg', 'deathIncrease', 'hospitalizedIncrease', 'negativeIncrease', 'positiveIncrease', 'totalTestResultsIncrease', 'hash']
+​
+
+Covid-19 also defines a function to get the data for a particular field possibly perCapita and/or averaged by Week:
+
+''' python
+def collectData(states,field,options={'perCapita':True,'averageByWeek':True}):
+'''
+e.g.
+''' python
+data = collectData(['MA'],'positiveIncrease',options={'perCapita':False,'averageByWeek':False})
+data[-10:]
+
+[(192, 213),
+ (193, 288),
+ (194, 199),
+ (195, 230),
+ (196, 303),
+ (197, 217),
+ (198, 234),
+ (199, 298),
+ (200, 359),
+ (201, 296)]
+'''
+
+Another useful function plots the items with the horizontal scale being the days of 2020
+''' python
+plotItems(
+    collectData(s,
+                'positiveIncrease',
+                options={'perCapita':False,'averageByWeek':False}),
+    'new cases per 10K')
+'''
--- covid19.py
+++ covid19.py
@@ -1,5 +1,10 @@
 import json
 import requests
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
 def get_covid_data():
     """returns list of daily covid data for the state"""
 
@@ -15,6 +20,16 @@ def get_covid_data():
         d['dateChecked'] = d['date']
     return (all_data,us_data)
 
+cdata = get_covid_data()
+len(cdata[0])
+
+dates = sorted(list({ x['date'] for x in cdata[0]}))
+dayOfPandemic={}
+count=22
+for d in dates:
+    dayOfPandemic[d]=count
+    count = count + 1
+
 def print_info(r):
     """
     r = {'date': 20200304,
@@ -29,7 +44,147 @@ def print_info(r):
     """
     print(r)
 
-#recipes = get_recipes('stew',ingredients=['lamb','cabbage'])
+    
+def averageData(L,k):
+    """ average data over k days"""
+    week = [0]*k
+    newL=[]
+    for x in L:
+        week = week[1:]+[x[1]]
+        newL += [(x[0],sum(week)/k)]
+    return newL
+
+
+def collectData(states,field,options={'perCapita':True,'averageByWeek':True}):
+    """ 
+        for a list of states and a field and an options object, return plottable data
+        the data is a list of tuples (dayOfYear,value)  
+        where the day of year goes from 1=1/1/2020 to 365=12/31/2020
+    """
+    perCapita = options['perCapita'] if 'perCapita' in options else False
+    averageByWeek = options['averageByWeek'] if 'averageByWeek' in options else False
+    if averageByWeek == True:
+        averageByWeek=7
+    pop=0
+    for st in states:
+        pop += statePop(st)
+    data={}
+    for d in cdata[0]:
+        s=d['state']
+        if s in states:
+            n = getField(d,field)
+            if d['date'] in data:
+                data[d['date']] += n
+            else:
+                data[d['date']] = n
+    z = sorted(data.items())
+    if perCapita:
+        z = [(dayOfPandemic[d[0]],d[1]*10000/pop) for d in z]
+    else:
+        z = [(dayOfPandemic[d[0]],d[1]) for d in z]
+ 
+
+    if averageByWeek:
+        z = averageData(z,averageByWeek)
+        
+    return z
+
+def statePop(state):
+    x = [d['Pop'] for d in stateData['data'] if state in states and d['State']==states[state]]
+    return x[0] if len(x)>0 else 100000000
+
+def plotItems(items,label):
+    plt.rcParams['figure.figsize'] = [12, 6]
+    """ plots the (day,value) pairs with the specified label"""
+    plt.plot([x[0] for x in items],[x[1] for x in items],label=label)
+    plt.xticks([0,30,60,90,120,150,180,210,240,270,300,330,360],'Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec Jan'.split(' '))
+
+
+def getField(x,field):
+    if field in x:
+        z = x[field]
+        if (z==None):
+            return 0
+        else:
+            return z
+    else:
+        return 0
+def getStateData(state,field):
+    sd = [(x['date'],getField(x,field)) for x in cdata[0] if x['state'] == state]
+    return sorted(sd)
+
+stateData = json.load(open("states.json","r"))
+
+states = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    #"AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    #"DC": "District Of Columbia",
+    #"FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    #"GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    #"MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    #"MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    #"PW": "Palau",
+    "PA": "Pennsylvania",
+    #"PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    #"VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+}
+
+northEast   = ['MA','CT','RI','VT','NH','ME']
+midAtlantic = ['NY','PA','NJ','DE','MD','VA','WV']
+southEast   = ['NC','SC','GA','FL','AL','MS','LA','AR','TN']
+southWest   = ['TX','OK','NM','CO','UT','AZ']
+midWest     = ['OH','KY','IN','IL','MI','WI','MO','IA','MN','KS','NE','SD','ND']
+west        = ['CA','NV','HI']
+northWest   = ['WY','MT','ID','OR','WA','AK']
+
 
 if __name__ == '__main__':
     state = input("What state do you want to see?")
@@ -39,3 +194,5 @@ if __name__ == '__main__':
         print_info(r)
         print("\n*****************\n")
     print('bye!')
+else:
+    print('done')
